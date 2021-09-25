42a0b75e56636c36b409ee585190e26dbe8a04cb
delet-dis <igor@efimov.ga>
2021-09-26 00:23:12 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Igor Efimov on 26.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
