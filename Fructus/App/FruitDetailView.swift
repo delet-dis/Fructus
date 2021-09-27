c5743c5219b5a20e42e283367ce384f85d9be8e5
delet-dis <igor@efimov.ga>
2021-09-27 16:51:16 +0700

    🍑  added fruit detail view

---
+++ Fructus/App/FruitDetailView.swift
@@ -0,0 +1,47 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Igor Efimov on 27.09.2021.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    
+    var fruit:Fruit
+    
+    var body: some View {
+        NavigationView{
+            ScrollView(.vertical, showsIndicators:false){
+                VStack(alignment: .center, spacing: 20){
+                    VStack(alignment: .leading, spacing:20) {
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColor[1])
+                        
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColor[1])
+                        
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                    }
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth:640, alignment: .center)
+                }
+            }
+        }
+    }
+}
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit:fruitsData[0])
+    }
+}
