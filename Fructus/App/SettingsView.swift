d03d97ca8ea1ab2dc82bb2857dac55fac45f0879
delet-dis <igor@efimov.ga>
2021-09-29 23:58:09 +0700

    🛬 added onboarding toggle

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SettingsView: View {
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding = false
 
     var body: some View {
         NavigationView {
@@ -30,12 +31,39 @@ struct SettingsView: View {
                             }
                     }
 
-                    GroupBox(label: SettingsLabelView(labelText: "Application info", labelImage: "apps.iphone")) {
+                    GroupBox(label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")) {
+                        Divider().padding(.vertical, 4)
 
+                        Text("If you wish, you can restart the application by toggle this switch in the box. That way it starts the onboarding process ansd you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(Color(
+                            UIColor.tertiarySystemBackground
+                        )
+                        .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous)))
+                    }
+
+                    GroupBox(label: SettingsLabelView(labelText: "Application info", labelImage: "apps.iphone")) {
                         SettingsRowView(name: "Developer", content: "delet-dis")
                         SettingsRowView(name: "Designer", content: "Robert Petras")
                         SettingsRowView(name: "Compatibility", content: "iOS 14")
-                        SettingsRowView(name: "Project github", linkLabel: "delet-dis's", linkDestination: "github.com/delet-dis")
+                        SettingsRowView(name: "Project github", linkLabel: "delet-dis", linkDestination: "github.com/delet-dis")
                         SettingsRowView(name: "SwiftUI", content: "2.0")
                         SettingsRowView(name: "Version", content: "1.1.0")
                     }
